e0799f06541e0191b728194b168a5e161ba602a9
hayden <haydenburger43@gmail.com>
2022-09-14 13:38:18 -0500

    added databases

---
+++ develop/employee.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS jobs;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE jobs (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT
+);
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id int 
+);
---
+++ develop/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department
+  (name)
+VALUES
+("joe bob")
+("jim boe")
+("tony stark")
+INSERT INTO jobs
+  (title, salary, department_id)
+VALUES
+("worker" "30,000" "1")
+("worker" "30,000" "1")
+("Manger" "300,000" "2")
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+("joe" "bob" "1" "1")
+("jim" "boe" "1" "1")
+("tony" "stark" "2" )
